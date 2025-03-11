342c705658da3a2b9f8e598b335caf6643432906
Dvvid <jose6dg@gmail.com>
2025-03-11 13:49:15 -0500

    se realizaron pruebas al codigo, en su ejecución se creo la tabla Category exitosamente

--- src/main/java/com/pragma/home360/application/services/CategoryService.java
+++ src/main/java/com/pragma/home360/application/services/CategoryService.java
@@ -1,4 +1,22 @@
-package com.pragma.home360.home.category.application.services;
+package com.pragma.home360.application.services;
 
+import com.pragma.home360.domain.model.Category;
+import com.pragma.home360.domain.ports.out.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public Category saveCategory(Category category) {
+        // Aquí puedes agregar lógica de negocio antes de guardar la categoría
+        return categoryRepository.save(category);
+    }
 }
--- src/main/java/com/pragma/home360/domain/model/Category.java
+++ src/main/java/com/pragma/home360/domain/model/Category.java
@@ -4,12 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.NotBlank;
 @Entity
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+
     private Long id;
+    @NotBlank(message = "El nombre de la categoría no puede estar vacío")
+    @Size(max = 50, message = "El nombre de la categoría no puede tener más de 50 caracteres")
     private String name;
+    @NotBlank(message = "La descripción de la categoría no puede estar vacía")
+    @Size(max = 90, message = "La descripción de la categoría no puede tener más de 90 caracteres")
     private String description;
 
     public Long getId() {
@@ -36,3 +43,5 @@ public class Category {
         this.description = description;
     }
 }
+
+
