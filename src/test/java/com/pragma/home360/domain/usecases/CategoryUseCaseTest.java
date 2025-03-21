b66a8af17fa50fdb37f2da7294f3cb64430a0bed
dvvid-g <giraldo.jose@correounivalle.edu.co>
2025-03-21 13:05:14 -0500

    se crea clase CategoryModelTest, se realiza prueba al modelo satisfactoriamente

---
+++ src/test/java/com/pragma/home360/domain/usecases/CategoryUseCaseTest.java
@@ -0,0 +1,25 @@
+package com.pragma.home360.domain.usecases;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryUseCaseTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void getCategories() {
+    }
+
+    @Test
+    void getCategoryByName() {
+    }
+}
