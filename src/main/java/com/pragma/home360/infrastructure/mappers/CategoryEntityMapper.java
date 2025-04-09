cc1e856fb79ac8901ea669b13cfbb1f717803b0d
dvvid-g <giraldo.jose@correounivalle.edu.co>
2025-04-09 11:48:01 -0500

    se agrega mappin en CategoryEntityMapper el cual corrige el bug de guardar datos nulos

--- src/main/java/com/pragma/home360/infrastructure/mappers/CategoryEntityMapper.java
+++ src/main/java/com/pragma/home360/infrastructure/mappers/CategoryEntityMapper.java
@@ -4,11 +4,13 @@ package com.pragma.home360.infrastructure.mappers;
 import com.pragma.home360.domain.model.CategoryModel;
 import com.pragma.home360.infrastructure.entities.CategoryEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CategoryEntityMapper {
+    @Mapping(target = "name", source = "name")
     CategoryEntity modelToEntity(CategoryModel categoryModel);
     CategoryModel entityToModel(CategoryEntity categoryEntity);
     List<CategoryModel> entityListToModelList(List<CategoryEntity> categories);
