d291cb9750fb041da50220f1626261c2d34a17d0
dvvid-g <giraldo.jose@correounivalle.edu.co>
2025-05-27 23:31:48 -0500

    se agrega endpoind para obtener una lista de usuarios registrados

--- src/main/java/com/pragma/home360/infrastructure/endpoints/rest/UserController.java
+++ src/main/java/com/pragma/home360/infrastructure/endpoints/rest/UserController.java
@@ -3,10 +3,12 @@ package com.pragma.home360.infrastructure.endpoints.rest;
 
 import com.pragma.home360.application.dto.request.SaveUserRequest;
 import com.pragma.home360.application.dto.response.SaveUserResponse;
+import com.pragma.home360.application.dto.response.UserResponse;
 import com.pragma.home360.application.services.UserService;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +23,11 @@ public class UserController {
     @PostMapping("/")
     public ResponseEntity<SaveUserResponse> save(@RequestBody SaveUserRequest userRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.saveUser(userRequest));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Page<UserResponse>> getUsers(Pageable pageable) {
+        return ResponseEntity.ok(userService.getUsers(pageable.getPageNumber(), pageable.getPageSize(), pageable.isPaged()));
     }
 
 
--- src/main/java/com/pragma/home360/infrastructure/mappers/DepartmentEntityMapper.java
+++ src/main/java/com/pragma/home360/infrastructure/mappers/DepartmentEntityMapper.java
@@ -3,7 +3,6 @@ package com.pragma.home360.infrastructure.mappers;
 import com.pragma.home360.domain.model.DepartmentModel;
 import com.pragma.home360.infrastructure.entities.DepartmentEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 import java.util.List;
 
